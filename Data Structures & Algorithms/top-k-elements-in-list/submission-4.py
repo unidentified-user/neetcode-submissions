812ffb7bc555ec517da855e1883ca9296e300a0f
Maksym Shovkun <35871746+unidentified-user@users.noreply.github.com>
2026-09-23 12:35:45 +0200

    Fix comment formatting for time complexity

--- Data Structures & Algorithms/top-k-elements-in-list/submission-4.py
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.py
@@ -1,5 +1,5 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # Counter + most_common - O(klogn)
+        # Counter + most_common - O(k log n)
         count = Counter(nums)
         return [item for item, _ in count.most_common(k)]
